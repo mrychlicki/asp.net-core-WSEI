c01f2d3b58ea14c54efeefd4779c63ce1c5365e1
mrychlicki <mateusz.rychlicki@microsoft.wsei.edu.pl>
2020-11-12 23:58:01 +0100

    Lab1

---
+++ WSEI_asp.netcore/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSEI_asp.netcore.Models;
+
+namespace WSEI_asp.netcore.Controllers
+{
+    public class CompanyController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            CompanyAddedViewModel viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharInName = company.Name.Length,
+                NumberOfCharInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+
+            };
+            return View("CompanyAdded", viewModel);
+        }
+    }
+}
---
+++ WSEI_asp.netcore/Models/CompanyAddedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSEI_asp.netcore.Models
+{
+    public class CompanyAddedViewModel
+    {
+        public int NumberOfCharInName { get; set; }
+        public int NumberOfCharInDescription { get; set; }
+        public bool IsHidden { get; set; }
+    }
+}
